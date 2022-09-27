613fca16674c96815a55fe82d6ba4f2f074a3053
zaki-coder <zakiazizi1841992@gmail.com>
2022-09-27 18:29:54 +0200

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Eum odit minus expedita odio iusto et illo fugiat rem dolor necessitatibus perspiciatis non cumque voluptate 
+        nihil, ab ipsam cupiditate veniam, sed tempora. Ex dicta est enim aliquid, odit aperiam labore rerum!</p>
+    </section>
+  )
+}
+
+export default About
